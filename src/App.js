778a7fb830cfa7ecf90393600e6f9002e62b5831
Soner-Ozciftci <ozciftcisoner@gmail.com>
2023-08-10 18:02:50 +0200

    fetch

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
+
+import { useState } from "react";
+import ' ./App.css';
+
 function App() {
+
+const [quotes , setQuotes]=useState("");
+const getQuote= ()={
+  fetch("https://type.fit/api/quotes")
+}
+
+
   return (
     <div className="App">
- <h1>React New Project</h1>
+
     </div>
   );
 }
