e542aae6041871ad2fe6a91f47b1b6eca603e5c5
Soner-Ozciftci <ozciftcisoner@gmail.com>
2023-08-10 18:11:48 +0200

    button add

--- src/App.js
+++ src/App.js
@@ -1,20 +1,24 @@
 
 import { useState } from "react";
-import ' ./App.css';
+import './App.css';
 
 function App() {
 
 const [quotes , setQuotes]=useState("");
-const getQuote= ()={
+const getQuote= ()=>{
   fetch("https://type.fit/api/quotes")
   .then((res)=>res.json())
-  .then((data)=>console.log(data))
+  .then((data)=>{
+  let randomNum = Math.floor(Math.random()* data.lenght)
+setQuotes(data[randomNum])
+});
 }
 
 
   return (
     <div className="App">
-
+      {quotes.text}
+<button onClick={getQuote}>Get Quote</button>
     </div>
   );
 }
