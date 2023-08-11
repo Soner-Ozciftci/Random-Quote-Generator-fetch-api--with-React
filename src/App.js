fa62f43af73307a3bc80d618f63a5b7f2bf03f07
Soner-Ozciftci <ozciftcisoner@gmail.com>
2023-08-11 15:56:10 +0200

    index edit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-
 import { useState } from "react";
-import './App.css';
+import "./App.css";
 
 function App() {
-
-const [quotes , setQuotes]=useState("");
-const getQuote= ()=>{
-  fetch("https://type.fit/api/quotes")
-  .then((res)=>res.json())
-  .then((data)=>{
-  let randomNum = Math.floor(Math.random()* data.lenght)
-setQuotes(data[randomNum])
-});
-}
+  const [quotes, setQuotes] = useState("");
+  const getQuote = () => {
+    fetch("https://type.fit/api/quotes")
+      .then((res) => res.json())
+      .then((data) => {
+        let randomNum = Math.floor(Math.random() * data.length);
+        setQuotes(data[randomNum]);
+      });
+  };
 
 
   return (
     <div className="App">
       {quotes.text}
-<button onClick={getQuote}>Get Quote</button>
+      <button onClick={getQuote}>Get Quote</button>
     </div>
   );
 }
