be96050eca6b9f84142c05c4de871b5ced76cfb3
Soner-Ozciftci <ozciftcisoner@gmail.com>
2023-08-11 16:21:45 +0200

    finish

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import { useState, useEffect } from "react";
 import "./App.css";
 
 function App() {
-  const [quotes, setQuotes] = useState("");
-  const getQuote = () => {
+
+  const [quotes, setQuotes]=useState("");
+  const getQuote = ()=>{
     fetch("https://type.fit/api/quotes")
-      .then((res) => res.json())
-      .then((data) => {
-        let randomNum = Math.floor(Math.random() * data.length);
+      .then((res)=>res.json())
+      .then((data)=>{
+        let randomNum = Math.floor(Math.random()* data.length);
         setQuotes(data[randomNum]);
       });
   };
@@ -18,8 +19,16 @@ function App() {
 
   return (
     <div className="App">
-      {quotes.text}
-      <button onClick={getQuote}>Get Quote</button>
+      <div className="quote">
+        <p>{quotes.text}</p>
+        <p>{quotes.author}</p>
+
+        <div className="btnContainer">
+          <button onClick={getQuote} className="btn">
+            Get Quote
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
